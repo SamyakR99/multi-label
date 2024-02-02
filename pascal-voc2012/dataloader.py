9eec66137db0a500a7d3a6439cea94b32e38bea0
Samyak <48352883+SamyakR99@users.noreply.github.com>
2024-02-02 01:25:31 -0600

    Update dataloader.py

--- pascal-voc2012/dataloader.py
+++ pascal-voc2012/dataloader.py
@@ -29,41 +29,25 @@ class VOCDataset(Dataset):
         return image, label_name
 
 # Load filenames
-train_image_names = np.load('formatted_train_images.npy')
-train_label_names = np.load('formatted_train_labels.npy')
-val_image_names = np.load('formatted_val_images.npy')
-val_label_names = np.load('formatted_val_labels.npy')
+train_image_names = np.load('/home/samyakr2/multilabel/data/pascal/formatted_train_images.npy')
+train_label_names = np.load('/home/samyakr2/multilabel/data/pascal/formatted_train_labels.npy')
+val_image_names = np.load('/home/samyakr2/multilabel/data/pascal/formatted_val_images.npy')
+val_label_names = np.load('/home/samyakr2/multilabel/data/pascal/formatted_val_labels.npy')
 
 # Define transformations
 transform = transforms.Compose([
-    transforms.Resize((256, 256)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
 ])
-
 
 
 path_to_images = '/home/samyakr2/multilabel/data/pascal/VOCdevkit/VOC2012/JPEGImages'
 train_dataset = VOCDataset(image_names=train_image_names, label_names=train_label_names, 
-                     image_dir=path_to_images, 
-                     transforms=transform)
+                     image_dir=path_to_images, transforms=transform)
 
 val_dataset = VOCDataset(image_names=val_image_names, label_names=val_label_names, 
-                     image_dir=path_to_images, 
-                     transforms=transform)
+                     image_dir=path_to_images, transforms=transform)
 
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-
-
-import matplotlib.pyplot as plt
-# Iterate over batches
-for images, labels in val_dataloader:
-    print(images.shape)
-    plt.imshow(images[0].permute(1,2,0))
-    plt.show()
-    print
-    print(labels[0])
-#     print(images, labels)
-    break
-    # Train your model with images and labels
-#     pass
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False)
